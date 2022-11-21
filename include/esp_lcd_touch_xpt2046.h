c83444182b6abd6387a8c445327a8ca0b3033687
Mike Dunston <m_dunston@comcast.net>
2022-11-21 11:35:23 -0800

    ESP-IDF v5.0-rc1 fix up.
    IDF v5.0 dropped esp_lcd_panel_io_spi_config_t :: flags.dc_as_cmd_phase.
    
    IDF v5.0 now also requires a value in esp_lcd_panel_io_spi_config_t :: dc_gpio_num (initailzier will default to GPIO 0 which is not ideal)

--- include/esp_lcd_touch_xpt2046.h
+++ include/esp_lcd_touch_xpt2046.h
@@ -6,7 +6,9 @@
 
 #pragma once
 
-#include "esp_lcd_panel_vendor.h"
+#include "esp_idf_version.h"
+#include "esp_lcd_touch.h"
+#include "esp_lcd_panel_io.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,6 +19,8 @@ extern "C" {
  *
  */
 #define ESP_LCD_TOUCH_SPI_CLOCK_HZ   (1 * 1000 * 1000)
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5,0,0)
 
 /**
  * @brief Communication SPI device IO structure
@@ -41,6 +45,30 @@ extern "C" {
             .lsb_first = 0                              \
         }                                               \
     }
+#else
+
+/**
+ * @brief Communication SPI device IO structure
+ *
+ */
+#define ESP_LCD_TOUCH_IO_SPI_XPT2046_CONFIG(touch_cs)   \
+    {                                                   \
+        .cs_gpio_num = (gpio_num_t)touch_cs,            \
+        .spi_mode = 0,                                  \
+        .pclk_hz = ESP_LCD_TOUCH_SPI_CLOCK_HZ,          \
+        .trans_queue_depth = 3,                         \
+        .on_color_trans_done = NULL,                    \
+        .user_ctx = NULL,                               \
+        .lcd_cmd_bits = 8,                              \
+        .lcd_param_bits = 8,                            \
+        .flags =                                        \
+        {                                               \
+            .dc_low_on_data = 0,                        \
+            .octal_mode = 0,                            \
+            .lsb_first = 0                              \
+        }                                               \
+    }
+#endif // IDF v5.0.0
 
 /**
  * @brief Create a new XPT2046 touch driver
